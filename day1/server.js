dacd580a00174982615bcc6f9f2a3653cb701f20
Alexis Valenciano <gabr.933@gmail.com>
2019-08-20 17:25:49 -0600

    Modified comments on server file

--- day1/server.js
+++ day1/server.js
@@ -1,5 +1,8 @@
-
-/*This is a basic setup server your can run it using node server.js */
+/*This is a basic setup server your can run it using node server.js
+    -Shows how to import files in nodejs
+    -How to answer http get request
+    -How to use a call back function on the completition of the server start(listen)
+*/
 const HTTP = require('http');
 const HOST = '127.0.0.1';
 const PORT = 666;
@@ -10,4 +13,4 @@ const server  = HTTP.createServer((req,res)=>{
 })
 server.listen(PORT,HOST,(()=>{
     console.log(`Server running on port ${PORT}`)
-}))
+}));
